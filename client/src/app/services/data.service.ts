b5ab27fb2aba917b1510876d140e87e8bd2f2b96
thienqle-fullstack <thienqle.vn@gmail.com>
2020-11-06 13:47:11 -0500

    change services

--- client/src/app/services/data.service.ts
+++ client/src/app/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,14 @@ export class DataService {
 
   private url = "http://localhost:3000"
   
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,private router : Router) { 
+    if(window.location.href.includes('herokuapp')) {
+      this.url = '';
+    } else {
+      this.url = "http://localhost:3000"
+    }
+
+  }
 
   getEmployees():Observable<any>{
     return this.http.get(this.url + "/api/employees");
